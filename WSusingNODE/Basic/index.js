c0bb1c880708e801eaed4b88c3c79c726eb4c832
Masuk <masukrahoman5@gmail.com>
2025-06-17 00:03:43 +0600

    Learn a little more

--- WSusingNODE/Basic/index.js
+++ WSusingNODE/Basic/index.js
@@ -27,9 +27,43 @@ wss.on("connection", (ws, req) => {
     });
 
     ws.send("Yeah I got your message");
+
+    ws.on("error", (error) => {
+      console.log("Client error Occurred", error);
+    });
   });
+  ws.on("close", (code, reason) => {
+    console.log(`Connection closed with code ${code} and reason ${reason}`);
+  });
+});
+
+wss.on("error", (error) => {
+  console.log("Server Error occurred", error);
 });
 
 server.listen(3000, () => {
   console.log("Server started at http://localhost:3000");
 });
+
+/*
+
+ws.CONNECTING -	0	= Still establishing connection
+ws.OPEN	- 1	= Connection is open (✅ can send data)
+ws.CLOSING -	2	= Connection is closing
+ws.CLOSED -	3	= Connection is closed
+*/
+
+/*
+In WebSocket (ws library), each connected client is represented by a WebSocket 
+object (ws), and all active clients are stored in wss.clients.
+
+JavaScript compares objects by reference — so client === ws checks if 
+they're the exact same object in memory.
+
+The condition client.readyState === ws.OPEN && client !== ws ensures 
+the server broadcasts only to connected clients, excluding the sender.
+
+This is the core logic behind implementing real-time group messaging or 
+broadcasting.
+
+*/
